3f4b06897fccc3a67d1fe662f4221e20d336d8ab
Vicenç García-Altés <vgaltes@thoughtworks.com>
2016-10-14 09:50:01 +0100

    Can place only drinks order failing

---
+++ CafeApp/src/CafeApp.Core.Tests/PlaceOrderTests.fs
@@ -0,0 +1,24 @@
+module PlaceOrderTests
+
+open NUnit.Framework
+open CafeAppTestsDSL
+open Domain
+open System
+open States
+open Commands
+open Events
+
+let tab = {Id = Guid.NewGuid(); TableNumber = 1}
+let coke = Drink{
+            MenuNumber = 1
+            Name = "Coke"
+            Price = 1.5m}
+let order = {Tab = tab; Foods = []; Drinks=[]}
+
+[<Test>]
+let ``Can place only drinks order`` () =
+    let order = {order with Drinks = [coke]}
+    Given (OpenedTab tab)
+    |> When (PlaceOrder order)
+    |> ThenStateShouldBe (PlaceOrder order)
+    |> WithEvents [OrderPlaced order]
